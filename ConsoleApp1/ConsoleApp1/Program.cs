562a32832e7ce45452c0ccc38828559aba3aac9d
Matic Brankovič <matic.brankovic@gmail.com>
2023-01-20 09:48:19 +0100

    vaja2
    git commit-m "Prvi commit za test"

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random nakljucno = new Random();
+        Console.WriteLine("zacetno ");
+        String beri = Console.ReadLine();
+        int denar = int.Parse(beri);
+        int zacentni = denar;
+        
+        Console.WriteLine("koliko stavis");
+        beri = Console.ReadLine();
+        int Pstava = int.Parse(beri);
+        int stava = Pstava;
+
+        int min = denar + 1;
+        int max = 0;
+        int umes = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            int rand = nakljucno.Next(36);
+
+            if (rand % 2 == 0)
+            {
+                denar = denar + stava;
+                Console.WriteLine("rdeca stanje je: " + denar);
+                Console.WriteLine("");
+                stava = Pstava;
+                if (max < denar)
+                {
+                    max = denar;
+                }
+
+                continue;
+
+            }
+            else
+            {
+                denar = denar - stava;
+                Console.WriteLine("crna stanje je: " + denar);
+                Console.WriteLine("");
+                stava = stava + stava;
+                if (min > denar)
+                {
+                    min = denar;
+                }
+
+            }
+
+        }
+        int konc = denar;
+        int razlika = konc - zacentni;
+        Console.WriteLine("Najmanj si imel: " + min);
+        Console.WriteLine("Največ si imel: " + max);
+        Console.WriteLine("Razlika: " + razlika);
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ vaja2/vaja2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaja2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald farmo ima iaiao."  );
+            Console.WriteLine("Na tej farmi osla dva iaiao");
+        }
+    }
+}
