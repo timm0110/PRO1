e39e406bcdd3f8feeb1e6d48ffd17bf48f1d1611
Matic Brankovič <matic.brankovic@gmail.com>
2023-01-30 10:51:17 +0100

    Karte vaja

---
+++ Karte/Karte/Barva.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karte
+{
+    enum Barva
+    {
+        srce, karo, pik, kriz
+    }
+
+    enum Vrednost
+    {
+        enka, dvojka, trojka, stirka, petka, sestka, sedemka, osemka, devetka, desetka, fant, dama, kralj 
+    }
+}
---
+++ Karte/Karte/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int barva = r.Next(4);
+            int vrednost = r.Next(13);
+            Console.WriteLine((Barva)barva + " " + (Vrednost)vrednost);
+            Console.ReadLine();
+        }
+    }
+}
